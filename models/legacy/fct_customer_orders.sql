1b3b0377f583f7451e460106d26898c9f4b53395
Melissa Bottomley <ametsanalytics@gmail.com>
2025-01-20 10:07:31 +0000

    add int_orders

--- models/legacy/fct_customer_orders.sql
+++ models/legacy/fct_customer_orders.sql
@@ -2,22 +2,7 @@
 
 with orders as (
 
-    select *,
-     row_number() over (partition by customer_id order by order_date, order_id) as user_order_seq
-    
-    from {{ ref("stg_jaffle_shop__orders") }}
-),
-
-
-customers as (
-
-    select 
-        customer_id,
-        full_name,
-        last_name as surname,
-        first_name as givenname
-
-    from {{ ref('stg_jaffle_shop__customers') }}
+   select * from {{ ref('int_orders') }}
 ),
 
 payments as (
@@ -31,6 +16,9 @@ customer_order_history as (
 
     select 
         customer_id,
+        full_name,
+        surname,
+        givenname,
         total_lifetime_value,
         first_order_date,
         order_count
@@ -47,13 +35,11 @@ final as (
         first_order_date,
         order_count,
         total_lifetime_value,
-        amount as order_value_dollars,
+        order_value_dollars,
         order_status,
-        payment_status
+        payments.payment_status
         
     from orders
-    inner join customers
-    on orders.customer_id = customers.customer_id
     inner join customer_order_history
     on orders.customer_id = customer_order_history.customer_id
     left outer join payments
