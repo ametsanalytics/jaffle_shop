d28101923997b692afd3147ab08c139a6bb9ebe4
Melissa Bottomley <ametsanalytics@gmail.com>
2025-01-20 09:50:46 +0000

    tidy up customer_order_history

--- models/intermediate/customer_order_history.sql
+++ models/intermediate/customer_order_history.sql
@@ -3,7 +3,7 @@
 with orders as (
 
     select *,
-     row_number() over (partition by customer_id order by order_date, order_id) as user_order_seq
+    row_number() over (partition by customer_id order by order_date, order_id) as user_order_seq
     
     from {{ ref("stg_jaffle_shop__orders") }}
 ),
@@ -28,9 +28,8 @@ payments as (
         amount as payment_amount
 
     from {{ ref('stg_stripe__payments') }}
+    where payment_status != 'fail'
 ),
-
--- logic
 
 final as (
 
@@ -39,22 +38,28 @@ final as (
             full_name,
             surname,
             givenname,
-            min(case when orders.order_status not in ('returned','return_pending') then order_date end) as first_non_returned_order_date,
-            max(case when orders.order_status not in ('returned','return_pending') then order_date end) as most_recent_non_returned_order_date,
-            coalesce(count(case when orders.order_status != 'returned' then 1 end),0) as non_returned_order_count,
+            min(valid_order_date) as first_non_returned_order_date,
+           
+            max(valid_order_date) as most_recent_non_returned_order_date,
+
+            coalesce(count(case when valid_order_date is not null then 1 end),0) as non_returned_order_count,
+
             sum(case 
-                when orders.order_status not in ('returned','return_pending') then payment_amount else 0 
-                end)/nullif(count(case when orders.order_status not in ('returned','return_pending') then 1 
+                when valid_order_date is not null then payment_amount else 0 
+                end)/nullif(count(case when valid_order_date is not null then 1 
                 end),0)
             as avg_non_returned_order_value,
-              array_agg(distinct orders.order_id) as order_ids,
+
+            array_agg(distinct orders.order_id) as order_ids,
             min(order_date) as first_order_date,
             coalesce(max(user_order_seq),0) as order_count,
+
             sum(case 
-                when orders.order_status not in ('returned','return_pending') 
-                then payment_amount else 0 
+                    when valid_order_date is not null
+                    then payment_amount else 0 
                 end) 
             as total_lifetime_value
+
 
         from customers
         inner join orders 
@@ -62,9 +67,9 @@ final as (
         left outer join payments 
         on orders.order_id = payments.order_id
 
-        where orders.order_status not in ('pending') and payments.payment_status != 'fail'
-
         group by customers.customer_id, customers.full_name, customers.surname, customers.givenname
+
+        
 
 )
 
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -3,6 +3,8 @@ select
     user_id as customer_id,
     order_date,
     status as order_status, 
-    _etl_loaded_at
+    _etl_loaded_at,
+    case when order_status not in ('returned','return_pending') then order_date end as valid_order_date 
+
 
 from {{ source('jaffle_shop', 'orders') }}
