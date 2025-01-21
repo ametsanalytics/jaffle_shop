715fbeebd4055ae5ac0d6f5900df5dd9a85ed98d
Melissa Bottomley <ametsanalytics@gmail.com>
2025-01-21 11:55:44 +0000

    fixes to window calcs and ordering

--- models/legacy/fct_customer_orders_pat.sql
+++ models/legacy/fct_customer_orders_pat.sql
@@ -45,20 +45,19 @@ customer_paid_orders as (
         orders.customer_id,
         order_placed_at,
         order_status,
-        p.total_amount_paid,
-        p.payment_finalized_date,
+        paid_orders.total_amount_paid,
+        paid_orders.payment_finalized_date,
         customer_first_name,
-        customer_last_name,
-        sum(total_amount_paid) over (partition by orders.customer_id) as customer_lifetime_value,
+        customer_last_name
+
+        /*
         min(order_placed_at) over (partition by orders.customer_id) as first_order_date,
         max(order_placed_at) over (partition by orders.customer_id) as most_recent_order_date,
-        count(orders.order_id) over (partition by orders.customer_id) as number_of_orders,
-        row_number() over (order by orders.order_id) as transaction_seq,
-        row_number() over (partition by orders.customer_id order by orders.order_id) as customer_sales_seq
-
+        count(orders.order_id) over (partition by orders.customer_id) as number_of_orders,*/
+        
     from orders
-    left join paid_orders p on orders.order_id = p.order_id
-    left join customers c on orders.customer_id = c.customer_id 
+    left join paid_orders  on orders.order_id = paid_orders.order_id
+    left join customers  on orders.customer_id = customers.customer_id 
 
 
 ),
@@ -74,14 +73,19 @@ final as (
         payment_finalized_date,
         customer_first_name,
         customer_last_name,
-        transaction_seq,
-        customer_sales_seq,
+
+        row_number() over (order by order_id ) as transaction_seq,
+        row_number() over (partition by customer_id order by order_id ) as customer_sales_seq,
+
         case 
-            when first_order_date = order_placed_at
-            then 'new' else 'return' 
+            when (
+            rank() over (partition by customer_id order by order_placed_at, order_id) = 1) 
+            then 'new'
+            else 'return'
         end as nvsr,
-        customer_lifetime_value,
-        first_order_date as fdos   
+
+        sum(total_amount_paid) over (partition by customer_id order by  order_placed_at, order_id) as customer_lifetime_value,
+        first_value(first_order_date) over (partition by customer_id order by order_placed_at, order_id) as fdos   
 
     from customer_paid_orders 
       
